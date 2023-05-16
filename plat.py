56bf0ac159d88bb7a281c45e5dcf52da75b7184f
Janahan Siva <57200038+ZeDUCC@users.noreply.github.com>
2023-05-16 16:19:09 -0400

    started new game

---
+++ plat.py
@@ -0,0 +1,15 @@
+import pygame
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main():
+
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
